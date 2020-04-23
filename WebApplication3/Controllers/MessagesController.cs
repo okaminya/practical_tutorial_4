2a92503e31e2067cf49bd0c352996d1f780b0f8c
okami <tristanraay@gmail.com>
2020-04-24 00:05:52 +0600

    Добавьте файлы проекта.

---
+++ WebApplication3/Controllers/MessagesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Collections.Generic;
+
+namespace WebApplication1.Controllers
+{
+    [Route("Say")]
+    public class MessagesController : Controller
+    {
+        [Route("{**message}")]
+        public IActionResult ShowMessage(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                ViewData["Message"] = "empty message";
+            }
+            else
+            {
+                ViewData["Message"] = message;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication3/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplication3.Data;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MoviesContext>(options =>
+            {
+                options.UseSqlite("Filename=movies.db");
+            });
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Hello}/{action=Index}/{id?}");
+            });
+
+            app.UseStaticFiles();
+
+            // app.UseMvcWithDefaultRoute(); the same as previous line
+
+            /*app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });*/
+        }
+    }
+}
